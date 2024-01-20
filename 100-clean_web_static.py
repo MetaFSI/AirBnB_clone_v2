531bcdb4d1ff3f426a996427353c9f0587b608b4
Mounir el fahsi <elfahssimounir7@gmail.com>
2024-01-20 19:52:09 +0100

    updated 10

--- 100-clean_web_static.py
+++ 100-clean_web_static.py
@@ -10,7 +10,7 @@ def do_clean(number=0):
     """Information about : Delete out-of-date archives.
 
     Args:
-        number (int): The number of archives to keep.
+        number (int): The number of archives to keep it.
 
     If number is 0 or 1, keeps only the most recent archive. If
     number is 2, keeps the most and second-most recent archives,
