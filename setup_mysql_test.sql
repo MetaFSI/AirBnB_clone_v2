263fb7aef604d34a41695fb815e8e37b2b15e36a
Mounir el fahsi <elfahssimounir7@gmail.com>
2024-01-20 19:52:57 +0100

    updated 12

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -1,4 +1,4 @@
---  Prepares a MySQL server for the project
+--  Prepares a MySQL server for project
 
 CREATE DATABASE IF NOT EXISTS hbnb_test_db;
 CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
